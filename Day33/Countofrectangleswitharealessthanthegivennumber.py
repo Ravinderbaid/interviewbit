d7dd2b4af3b11a09d0bc0a11328ee4b0eef40e78
Ravinder Baid <ravinder.baid@incedoinc.com>
2020-04-20 21:01:46 +0530

    Added code for another question for day33

---
+++ Day33/Countofrectangleswitharealessthanthegivennumber.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        i=0
+        j=len(A)-1
+        c=0
+        while(i<=j):
+            if A[i]*A[j]<B:
+                c+=((j-i)*2)+1
+                i+=1
+            else:
+                j-=1
+        return c%(10**9+7)
