9a93e7efa23dd2b31f7aefdb50e544f335ae1ae1
Ravinder Baid <ravinder.baid@incedoinc.com>
2020-04-20 20:35:34 +0530

    Updated Day33 1 question

---
+++ Day33/Countofpairswiththegivensum.py
@@ -0,0 +1,18 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        i=0
+        j=len(A)-1
+        c=0
+        while(i<j):
+            if A[i]+A[j] == B:
+                c+=1
+                i+=1
+                j-=1
+            elif A[i]+A[j]>B:
+                j-=1
+            else:
+                i+=1
+        return c
