832431546b77c66994a8db40b61a6fb912124e9c
Ravinder Baid <ravinder.baid@incedoinc.com>
2020-04-22 13:47:45 +0530

    Added code for assignment of day33

---
+++ Day33/3sum.py
@@ -0,0 +1,19 @@
+class Solution:
+	# @param A : list of integers
+	# @param B : integer
+	# @return an integer
+	def threeSumClosest(self, A, B):
+	    A.sort()
+	    current = 10**8
+	    for i in range(len(A)-2):
+	        p = i+1
+	        q = len(A)-1
+	        while(p<q):
+	            x = A[p]+A[q]+A[i]
+	            if abs(B - x) <= abs(B - current):
+	                current = x
+	            if x>B:
+	                q-=1
+	            else:
+	                p+=1
+	    return current
---
+++ Day33/Findsub-arraywiththegivensum.py
@@ -0,0 +1,26 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return a list of integers
+    def solve(self, A, B):
+        prefix_arr=[A[0]]
+        for i in A[1:]:
+           prefix_arr.append(prefix_arr[-1]+i) 
+        i=0
+        j=0
+        flag = False
+        while(i<len(A) and j<len(A)):
+            p=prefix_arr[j]
+            if flag:
+                p-=prefix_arr[i-1]
+            if p==B:
+                if i==j:
+                    return [A[i]]
+                else:
+                    return A[i:j+1]
+            elif p<B:
+                j+=1
+            else:
+                flag=True
+                i+=1
+        return [-1]
