125aa66b27b0a6440543c215e4640de9fbed8ae4
Ravinder Baid <ravinder.baid@incedoinc.com>
2020-04-24 02:20:20 +0530

    Day33 complete hurray :)

--- Day33/CountSubarrays.py
+++ Day33/CountSubarrays.py
@@ -2,17 +2,18 @@ class Solution:
     # @param A : list of integers
     # @return an integer
     def solve(self, A):
-        A.sort()
         i=0
         j=0
         s = {}
         ans=0
+        p=j
         while(i<len(A)):
-            while(j<len(A) and not s.get(A[j],None)):
-                s[A[j]]=1
+            while(j<len(A) and s.get(A[j],True)):
+                s[A[j]]=False
                 j+=1
-            ans+=((j-i)*(j-i+1))//2
+            ans+=(j-i)
             s.pop(A[i])
             i+=1
-        return (ans%10**9+7)
+            
+        return (ans%(10**9+7))
                 
