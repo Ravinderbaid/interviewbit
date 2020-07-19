4c3abe536c6f459e5944fd3188e0aa6980cea807
Ravinder Baid <ravinderbaid@gmail.com>
2020-07-19 16:24:23 +0530

    Fixed formatting for day 29

--- Day29/triplets.py
+++ Day29/triplets.py
@@ -1,34 +1,32 @@
 class Solution:
-	# @param A : list of strings
-	# @return an integer
-	def solve(self, A):
-	    A=[float(i) for i in A]
-	    a=A[0]
-	    b=A[1]
-	    c=A[2]
-	    
-	    for i in range(3,len(A)):
-	        if 1<(a+b+c)<2:
-	            return 1
-	        n=A[i]
-	        if (a+b+c)>=2:
-	            if a>b and a>c:
-	                a=n
-	            elif b>c:
-	                b=n
-	            else:
-	                c=n
-	        else:
-	            if a<b and a<c:
-	                a=n
-	            elif b<c:
-	                b=n
-	            else:
-	                c=n
-	    
-	    if 1<(a+b+c)<2:
-	        return 1
-	       
-	    return 0 
-	                
+    # @param A : list of strings
+    # @return an integer
+    def solve(self, A):
+        A = [float(i) for i in A]
+        a = A[0]
+        b = A[1]
+        c = A[2]
 
+        for i in range(3, len(A)):
+            if 1 < (a + b + c) < 2:
+                return 1
+            n = A[i]
+            if (a + b + c) >= 2:
+                if a > b and a > c:
+                    a = n
+                elif b > c:
+                    b = n
+                else:
+                    c = n
+            else:
+                if a < b and a < c:
+                    a = n
+                elif b < c:
+                    b = n
+                else:
+                    c = n
+
+        if 1 < (a + b + c) < 2:
+            return 1
+
+        return 0
