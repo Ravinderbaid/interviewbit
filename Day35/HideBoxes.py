0d7c003bd3b60f3da4bb48fa79c468a9322e67e7
Ravinder Baid <ravinderbaid@gmail.com>
2020-07-28 22:39:13 +0530

    Day 35 code for interviewbit.

--- Day35/HideBoxes.py
+++ Day35/HideBoxes.py
@@ -3,17 +3,16 @@ class Solution:
     # @return an integer
     def solve(self, A):
         A.sort()
-        n=len(A)
-        j=n//2
-        i=0
-        ans=n
-        while(i<n//2 and j<n):
-            if A[i]*2 <=A[j]:
-                ans-=1
-                j+=1
-                i+=1
+        n = len(A)
+        j = n // 2
+        i = 0
+        ans = n
+        while i < n // 2 and j < n:
+            if A[i] * 2 <= A[j]:
+                ans -= 1
+                j += 1
+                i += 1
             else:
-                j+=1
-                
-        return ans
+                j += 1
 
+        return ans
--- Day35/swapElements.py
+++ Day35/swapElements.py
@@ -3,21 +3,21 @@ class Solution:
     # @param B : list of list of integers
     # @return a list of integers
     def solve(self, A, B):
-        Ad=list(A)
-        n=len(A)
-        mxi=max(A)
-        for i in range(1,n+1):
-            mx=Ad[i-1]
-            for j in range(i,n+1,i):
-                Ad[j-1]=max(Ad[j-1],A[i-1])
-                mx=max(mx,A[j-1])
-            Ad[i-1]=mx
-        ans=[]
+        Ad = list(A)
+        n = len(A)
+        mxi = max(A)
+        for i in range(1, n + 1):
+            mx = Ad[i - 1]
+            for j in range(i, n + 1, i):
+                Ad[j - 1] = max(Ad[j - 1], A[i - 1])
+                mx = max(mx, A[j - 1])
+            Ad[i - 1] = mx
+        ans = []
         for q in B:
-            if q[1]==0:
-                ans.append(A[q[0]-1])
-            elif q[1]==1:
-                ans.append(Ad[q[0]-1])
+            if q[1] == 0:
+                ans.append(A[q[0] - 1])
+            elif q[1] == 1:
+                ans.append(Ad[q[0] - 1])
             else:
                 ans.append(mxi)
         return ans
