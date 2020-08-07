0e9cac6c0bbff49f778056a3b2765109721b2140
Ravinder Baid <ravinderbaid@gmail.com>
2020-08-07 21:32:59 +0530

    Fixed code for day 71 bulbs.py

--- Day71/Bulbs.py
+++ Day71/Bulbs.py
@@ -1,19 +1,18 @@
 class Solution:
-	# @param A : list of integers
-	# @return an integer
-	def bulbs(self, A):
-	    press=0
-	    for i in range(len(A)):
-	        if press%2:
-	            if A[i]==0:
-	                continue
-	            else:
-	                press+=1
-	        else:
-	            if A[i]==1:
-	                continue
-	            else:
-	                press+=1
-	    
-	    return press
+    # @param A : list of integers
+    # @return an integer
+    def bulbs(self, A):
+        press = 0
+        for i in range(len(A)):
+            if press % 2:
+                if A[i] == 0:
+                    continue
+                else:
+                    press += 1
+            else:
+                if A[i] == 1:
+                    continue
+                else:
+                    press += 1
 
+        return press
